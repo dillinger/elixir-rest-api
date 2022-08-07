7e696acb9879f8ac1a1f08057825c16e060390d8
Dmytro Paniuta <d.paniuta@sellerlogic.dev>
2022-08-07 14:37:57 +0200

    Initial commit 2

---
+++ lib/rest_api/router.ex
@@ -0,0 +1,26 @@
+defmodule RestApi.Router do
+  # Bring Plug.Router module into scope
+  use Plug.Router
+
+  # Attach the Logger to log incoming requests
+  plug(Plug.Logger)
+
+  # Tell Plug to match the incoming request with the defined endpoints
+  plug(:match)
+
+  plug(Plug.Parsers,
+    parsers: [:json],
+    pass: ["application/json"],
+    json_decoder: Jason
+  )
+
+  plug(:dispatch)
+
+  get "/" do
+    send_resp(conn, 200, "OK")
+  end
+
+  match _ do
+    send_resp(conn, 404, "Not Found")
+  end
+end
