038a83e89c22e9fada032e722399a1afdab05555
Dmytro Paniuta <pdillinger@gmail.com>
2022-08-07 15:21:43 +0200

    Add mongodb_driver

--- mix.exs
+++ mix.exs
@@ -24,8 +24,7 @@ defmodule RestApi.MixProject do
     [
       {:plug_cowboy, "~> 2.5"},
       {:jason, "~> 1.3"},
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:mongodb_driver, "~> 0.8"}
     ]
   end
 end
