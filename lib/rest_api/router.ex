98678f566ab401552d1f63841c40822ad4e1fc1d
Dmytro Paniuta <pdillinger@gmail.com>
2022-08-07 16:23:43 +0200

    Add POST /post endpoint

--- lib/rest_api/router.ex
+++ lib/rest_api/router.ex
@@ -40,6 +40,30 @@ defmodule RestApi.Router do
     |> send_resp(200, posts)
   end
 
+  post "/post" do
+    case conn.body_params do
+      %{"name" => name, "content" => content} ->
+        case Mongo.insert_one(:mongo, "Posts", %{"name" => name, "content" => content}) do
+          {:ok, user} ->
+            doc = Mongo.find_one(:mongo, "Posts", %{_id: user.inserted_id})
+
+            post =
+              JSON.normaliseMongoId(doc)
+              |> Jason.encode!()
+
+            conn
+            |> put_resp_content_type("application/json")
+            |> send_resp(200, post)
+
+          {:error, _} ->
+            send_resp(conn, 500, "Somthing went wrong")
+        end
+
+      _ ->
+        send_resp(conn, 400, '')
+    end
+  end
+
   match _ do
     send_resp(conn, 404, "Not Found")
   end
